2e6e88408f60ecaf84d8e2c768284e9fbb7b9da6
William Ward <wcward3302@eagle.fgcu.edu>
2024-04-17 13:00:06 -0400

    Weekly Ads class V1

--- src/test/java/Weeklyads.java
+++ src/test/java/Weeklyads.java
@@ -1,64 +1,72 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class Weeklyads {
 
-    // location xpath = /html/body/div[2]/header/div[1]/div/div[2]/div/div/div[2]/div/div/button
-
     public WebDriver chrome_driver;
 
-    // Before testing, need to open a window and navigate to testing website
-    @BeforeTest
+    @Test (priority = 1)
     void open_browser() throws InterruptedException{
 
         chrome_driver = new ChromeDriver();
         chrome_driver.manage().window().maximize();
-        chrome_driver.get("https://publix.com/");
+        chrome_driver.get("https://www.publix.com");
+        Thread.sleep(1000);
+
+    }
+
+
+    @Test (priority = 2)
+    void go_to_weekly_flyer() throws InterruptedException{
+        chrome_driver.findElement(By.xpath("/html/body/div[2]/header/div[1]/div/div[2]/div/div/div[2]/div/ul/li[5]/a")).click();
+        Thread.sleep(2000);
+
+        //check if page is correct
+        Assert.assertEquals(chrome_driver.getTitle(), "Weekly Ad | Publix Super Markets", "Incorrect page");
+    }
+
+    @Test (priority = 3)
+    void scroll_flyer() throws InterruptedException{
 
         // Will run into issues if location is not selected at this point
-        chrome_driver.findElement(By.xpath("/html/body/div[2]/header/div[1]/div/div[2]/div/div/div[2]/div/div/button")).click();
+        
+        chrome_driver.findElement(By.xpath("/html/body/div[1]/header/div[1]/div/div[2]/div/div/div[2]/div/div/button")).click();
         Thread.sleep(1000);
         // end location input a string as then return to fetch 
         chrome_driver.findElement(By.name("search")).sendKeys("Estero" + Keys.RETURN);
         Thread.sleep(1000);
-        chrome_driver.findElement(By.xpath("/html/body/div[2]/header/div[1]/div/div[2]/div/div/div[1]/div[2]/div[1]/div/div/div[2]/div[2]/div/div/div/div[3]/ul/li[1]/div/div/div[2]/div[1]/div/button")).click();
-        Thread.sleep(3000);
-        //refresh page to prevent elemets going stale in the DOM
-        chrome_driver.navigate().refresh();
+        // click select location
+        chrome_driver.findElement(By.xpath("/html/body/div[1]/header/div[1]/div/div[2]/div/div/div[1]/div[2]/div[1]/div/div/div[2]/div[2]/div/div/div/div[3]/ul/li[1]/div/div/div[2]/div[1]/div/button")).click();
+        Thread.sleep(9000);
+
+        //change to list view
+        chrome_driver.findElement(By.xpath("/html/body/div[1]/section/div[3]/div/button[2]")).click();
+        Thread.sleep(1000);
+
+        // wait and scroll down to see flyer window
+        Thread.sleep(1000);
+        JavascriptExecutor js = (JavascriptExecutor) chrome_driver;
+        js.executeScript("window.scrollBy(0,300)", "");
+
+        Thread.sleep(1000);
+        js.executeScript("window.scrollBy(0,600)", "");
+        Thread.sleep(1000);
+        js.executeScript("window.scrollBy(0,900)", "");
+        Thread.sleep(1000);
+        js.executeScript("window.scrollBy(0,1200)", "");
+        Thread.sleep(1000);
 
     }
 
-    // After testing, need to close and quit the driver since no longer in use
-    @AfterTest
+    @Test (priority = 4)
     void close_browser() throws InterruptedException {
         Thread.sleep(3000);
         chrome_driver.close();
         chrome_driver.quit();
-    }
-
-    @Test (priority = 1)
-    void scrollFlyer() throws InterruptedException{
-        Thread.sleep(2000);
-        chrome_driver.findElement(By.xpath("/html/body/div[1]/header/div[1]/div/div[1]/ul/li[5]/a")).click();
-        Thread.sleep(2000);
-        
-
-    }
-
-    @Test (priority = 2)
-    void switchToList() throws InterruptedException {
-
-    }
-
-
-    @Test (priority = 3)
-    void viewItem() throws InterruptedException {
-    
     }
 }
